117a4eaac2c2569dd940233720e324039baa0036
ItzKaguya 2nd <106611054+MizuNotCool@users.noreply.github.com>
2022-07-01 12:29:59 +0800

    a10s : Update COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_a10s.mk
 
 COMMON_LUNCH_CHOICES := \
-     lineage_a10-userdebug \
-     lineage_a10-eng \
-     lineage_a10-user
+     lineage_a10s-userdebug \
+     lineage_a10s-eng \
+     lineage_a10s-user
