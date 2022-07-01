6fbbfba717433a55ee20aedbc8516bbd1f5e2790
ItzKaguya 2nd <106611054+MizuNotCool@users.noreply.github.com>
2022-07-01 10:40:05 +0800

    a10s : Update BoardConfig.mk and add Target Arch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,18 @@
 DEVICE_PATH := device/samsung/a10s
 BOARD_VENDOR := samsung
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_USES_64_BIT_BINDER := true
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-04-01
+
+# Android Verified Boot
+BOARD_AVB_ENABLE := false
+BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
