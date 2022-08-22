95a9b945baf4485b426038f0f1e186165bdf1ab3
crazyads69 <leminhtri11a1@gmail.com>
2022-08-23 06:53:13 +0700

    Some Minor Change

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -12,13 +12,18 @@ def IncrementalOTA_InstallEnd(info):
 
 def AddImage(info, basename, dest):
   name = basename
-  data = info.input_zip.read("IMAGES/" + basename)
+  path = "IMAGES/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
   PatchVendor(info)
   info.script.Print("Patching firmware images...")
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta")
 
 def PatchVendor(info):
